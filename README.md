ea4c73b73e0679bb837b89c2e8e4c1a982a7d724
adityaazizi <azizi.business@gmail.com>
2024-08-15 21:49:55 +0700

    new files

--- README.md
+++ README.md
@@ -1,1 +1,95 @@
-# vehicle-counting-and-speed-estimation-using-yolov8-and-byte-track
+# Vehicle Counting and Speed Estimation using YOLOv10
+
+This project implements a vehicle counting and speed estimation system using the YOLOv10 object detection model. The system processes video input to detect vehicles, track their movement, count, and estimate their speed.
+
+## Project Structure
+
+```bash
+├── sample
+│   ├── input
+│   │   └── video.mp4               # Sample input video file
+│   └── output
+│       └── video.mp4               # Output video file with bounding boxes and speed annotations
+├── weight
+│   └── yolov10n.pt                 # YOLOv10 model weights
+├── main.py                         # Main script for vehicle detection and speed estimation
+├── README.md                       # Project documentation
+└── requirements.txt                # Python dependencies
+```
+
+## How it Works
+
+1. Model: The project uses the YOLOv10 model for vehicle detection. The model is loaded using the ultralytics library and runs on the provided video input.
+2. View Transformation: The ViewTransformer class handles the transformation of points between source and target perspectives, crucial for accurate speed estimation.
+3. Tracking, Count and Speed Estimation:
+
+- The detected vehicles are tracked across frames using the ByteTrack algorithm from the supervision library.
+- The vehicle only detected when entering the polygon zone (red line) as can see in Image 1.
+- The line counter as can see on Image 1 (white line) will triggered when the vehicle pass the line from every direction.
+- Speed is estimated by measuring the distance traveled by each vehicle in the transformed view and dividing it by the time taken.
+- Output: The final output is a video with bounding boxes and speed annotations for each detected vehicle and counter vehicle from each directions as can see in Image 3.
+
+![Image 1](assets/image-2.png)
+![Image 2](assets/image-1.png)
+![Image 3](assets/image-3.png)
+
+## Getting Started
+
+### Prerequisites
+
+- Python 3.x
+
+### Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/adityaazizivehicle-counting-and-speed-estimation-using-yolov10.git
+cd vehicle-counting-and-speed-estimation-using-yolov10
+```
+
+2. Install the required dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+3. Run the project:
+
+```bash
+python main.py
+```
+
+## Configuration
+
+If you want to run with your own video you can set this configurations:
+
+- SOURCE_VIDEO_PATH: Path to the input video file.
+- TARGET_VIDEO_PATH: Path to the output video file.
+- MODEL: Path to the YOLOv10 model weights.
+- CONFIDENCE_THRESHOLD: Confidence threshold for vehicle detection.
+- MATCH_THRESHOLD: Threshold for tracking match.
+- IOU_THRESHOLD: Intersection Over Union threshold for Non-Maximum Suppression.
+- MODEL_RESOLUTION: Resolution for the YOLOv10 model.
+
+```bash
+SOURCE = np.array([
+    # source polygon zone
+])
+
+TARGET_WIDTH = '' # real width of the road
+TARGET_HEIGHT = '' # real length of the road
+
+TARGET = np.array([
+    # target polygon zone
+])
+
+# line counter point
+LINE_START = sv.Point()
+LINE_END = sv.Point()
+```
+
+## Acknowledgments
+
+- [YOLOv10 by Ultralytics](https://docs.ultralytics.com/models/yolov10/)
+- [Supervision by Roboflow](https://supervision.roboflow.com/latest/)
--- main.py
+++ main.py
@@ -7,6 +7,45 @@ from ultralytics import YOLO
 from collections import defaultdict, deque
 
 
+CURRENT_DIRECTORY = os.getcwd()
+SOURCE_VIDEO_PATH = os.path.join(
+    CURRENT_DIRECTORY,
+    'sample/input/video.mp4',
+)  # add your video path
+TARGET_VIDEO_PATH = os.path.join(
+    CURRENT_DIRECTORY,
+    'sample/output/video.mp4',
+)  # add your video path
+MODEL = os.path.join(
+    CURRENT_DIRECTORY,
+    'weight/yolov10n.pt',
+)
+CONFIDENCE_THRESHOLD = 0.25
+MATCH_THRESHOLD = 0.8
+IOU_THRESHOLD = 0.5
+MODEL_RESOLUTION = 1280
+
+SOURCE = np.array([
+    [1252, 787],
+    [2298, 803],
+    [5039, 2159],
+    [-550, 2159]
+])
+
+TARGET_WIDTH = 25
+TARGET_HEIGHT = 250
+
+TARGET = np.array([
+    [0, 0],
+    [TARGET_WIDTH - 1, 0],
+    [TARGET_WIDTH - 1, TARGET_HEIGHT - 1],
+    [0, TARGET_HEIGHT - 1],
+])
+
+LINE_START = sv.Point(0, 1250)
+LINE_END = sv.Point(3840, 1250)
+
+
 class ViewTransformer:
     def __init__(self, source: np.ndarray, target: np.ndarray) -> None:
         source = source.astype(np.float32)
@@ -22,44 +61,30 @@ class ViewTransformer:
         return transformed_points.reshape(-1, 2)
 
 
-CURRENT_DIRECTORY = os.getcwd()
-SOURCE_VIDEO_PATH = ''  # add your video path
-TARGET_VIDEO_PATH = ''  # add your video path
-MODEL = os.path.join(CURRENT_DIRECTORY, 'weight/yolov8x.pt')
-
-CONFIDENCE_THRESHOLD = ''
-MATCH_TRESHOLD = ''
-IOU_THRESHOLD = ''
-MODEL_RESOLUTION = ''
-
-SOURCE = np.array([
-    # your current polygon here
-])
-
-TARGET_WIDTH = ''
-TARGET_HEIGHT = ''
-
-TARGET = np.array([
-    # your target polygon here
-])
-
 model = YOLO(MODEL)
 model.fuse()
 
 video_info = sv.VideoInfo.from_video_path(video_path=SOURCE_VIDEO_PATH)
 byte_track = sv.ByteTrack(
     frame_rate=video_info.fps,
-    track_thresh=CONFIDENCE_THRESHOLD,
-    match_thresh=MATCH_TRESHOLD,
-    track_buffer=video_info.fps
+    track_activation_threshold=CONFIDENCE_THRESHOLD,
+    minimum_matching_threshold=MATCH_THRESHOLD,
+    lost_track_buffer=video_info.fps
 )
-thickness = sv.calculate_dynamic_line_thickness(
+
+polygon_zone = sv.PolygonZone(
+    polygon=SOURCE,
+)
+
+line_zone = sv.LineZone(start=LINE_START, end=LINE_END)
+
+thickness = sv.calculate_optimal_line_thickness(
     resolution_wh=video_info.resolution_wh
 )
-text_scale = sv.calculate_dynamic_text_scale(
+text_scale = sv.calculate_optimal_text_scale(
     resolution_wh=video_info.resolution_wh
 )
-bounding_box_annotator = sv.BoundingBoxAnnotator(
+box_annotator = sv.BoxAnnotator(
     thickness=thickness,
     color_lookup=sv.ColorLookup.TRACK
 )
@@ -68,14 +93,16 @@ label_annotator = sv.LabelAnnotator(
     text_thickness=thickness,
     color_lookup=sv.ColorLookup.TRACK
 )
-polygon_zone = sv.PolygonZone(
-    polygon=SOURCE, frame_resolution_wh=video_info.resolution_wh
+line_zone_annotator = sv.LineZoneAnnotator(
+    thickness=thickness,
+    text_thickness=thickness,
+    text_scale=text_scale,
 )
 
 view_transformer = ViewTransformer(source=SOURCE, target=TARGET)
 coordinates = defaultdict(lambda: deque(maxlen=video_info.fps))
 
-fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+fourcc = cv2.VideoWriter_fourcc(*'avc1')
 vid_writer = cv2.VideoWriter(
     TARGET_VIDEO_PATH,
     fourcc, video_info.fps,
@@ -91,13 +118,15 @@ for _ in tqdm(range(video_info.total_frames), desc="Rendering videos with Boundi
         frame,
         imgsz=MODEL_RESOLUTION,
         verbose=False,
-        device=''  # your device here
+        device='mps'
     )[0]
 
     detections = sv.Detections.from_ultralytics(result)
     detections = detections[polygon_zone.trigger(detections)]
     detections = detections.with_nms(IOU_THRESHOLD)
     detections = byte_track.update_with_detections(detections)
+
+    line_zone.trigger(detections)
 
     points = detections.get_anchors_coordinates(
         anchor=sv.Position.BOTTOM_CENTER
@@ -118,15 +147,20 @@ for _ in tqdm(range(video_info.total_frames), desc="Rendering videos with Boundi
         labels.append(f"{int(speed)} km/h")
 
     annotated_frame = frame.copy()
-    annotated_frame = bounding_box_annotator.annotate(
+    annotated_frame = box_annotator.annotate(
         scene=annotated_frame,
         detections=detections
+    )
+    annotated_frame = line_zone_annotator.annotate(
+        annotated_frame,
+        line_counter=line_zone
     )
     annotated_frame = label_annotator.annotate(
         scene=annotated_frame,
         detections=detections,
         labels=labels
     )
+
     vid_writer.write(annotated_frame)
 
 cap.release()
